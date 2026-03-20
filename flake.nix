8fcebbd2e28a9168e2563dde4fcf98d69aa2fe1a
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-19 21:56:34 -0400

    ci: add vimdoc check with runtime tag auto-discovery
    Problem: no vimdoc linting in CI meant tag reference errors went
    undetected.
    
    Solution: Add vimdoc-language-server `check` job. Runtime tags are
    auto-discovered by default, resolving nvim builtin references without
    needing explicit flags.

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    vimdoc-language-server.url = "github:barrettruth/vimdoc-language-server";
   };
 
   outputs =
     {
       nixpkgs,
       systems,
+      vimdoc-language-server,
       ...
     }:
     let
@@ -24,8 +26,10 @@
           packages = [
             pkgs.prettier
             pkgs.stylua
+            pkgs.neovim
             pkgs.selene
             pkgs.lua-language-server
+            vimdoc-language-server.packages.${pkgs.system}.default
           ];
         };
       });
